9f9cff2d0baa257f78b18ed2ece97e90c745f411
Rick Pfahl <pfahlr@gmail.com>
2019-07-30 13:51:26 -0400

    settings refactor

--- scripts/_vars.inc.sh
+++ scripts/_vars.inc.sh
@@ -1,8 +1,5 @@
-###################################
-#---------------------------------#
-# setup system specific variables #
-#---------------------------------#
-###################################
+source _settings.inc.sh
+
 continueyn() {
   local answer=''
   echo "continue[y/n]"
@@ -11,34 +8,9 @@ continueyn() {
 	  return 1
   elif [[ $answer == 'n' ]]; then
 	  exit 0
-  else  
+  else
 	  echo "I don't understand"
 	  continueyn
   fi
 
 }
-
-echo "setting system specific variables"
-
-target_device=/dev/sda
-echo "target device: $target_device"
-
-efinum=1
-swpnum=2
-sysnum=3
-
-system_part=${target_device}$sysnum
-echo "system partition: $system_part"
-
-efi_part=${target_device}${efinum}
-echo "efi partition: $efi_part"
-
-swap_part=${target_device}${swapnum}
-echo "swap partition: $swp_part"
-
-#fstype (e.g., ext4, btrfs, xfs, f2fs, zfs)
-system_part_fstype=btrfs
-echo "system filesystem format: $system_part_fstype"
-
-
-
