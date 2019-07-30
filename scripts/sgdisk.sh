43a5641190976f6584f0f2202d875d6b3cfe4240
Rick Pfahl <pfahlr@gmail.com>
2019-07-30 18:55:46 -0400

    syntax error

--- scripts/sgdisk.sh
+++ scripts/sgdisk.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 #clean any previous failed runs
 
-if[[ $system_part_fstype == "btrfs" ]]; then
+if [[ $system_part_fstype == "btrfs" ]]; then
   umount /mnt/boot
   umount /mnt/home
   umount /mnt/.snapshots
@@ -11,8 +11,12 @@ umount /mnt && cryptsetup close /dev/mapper/encsys
 swapoff -a && cryptsetup close /dev/mapper/swap
 
 source _vars.inc.sh
+echo "############### ALL TARGET PARTITIONS UNMOUNTED AND CLOSED ################"
+
+continueyn
 
 echo "clear and create new partition table"
+
 sgdisk --clear
 sgdisk --mbrtogpt
 sgdisk --zap-all $target_device
@@ -22,8 +26,12 @@ sgdisk --clear \
   --new=3:0:+250GiB --typecode=8309 --change-name=3:ENCSYS\
   $target_device
 sgdisk --sort
-echo "partition table created"
+
+echo ".......partition table created!"
+
 gdisk -l $target_device
+
+echo ".......end of partition table"
 
 #/dev/sda1 : start=        2048, size=     1024000, type=ef
 #/dev/sda2 : start=   608258048, size=    16777216, type=82
