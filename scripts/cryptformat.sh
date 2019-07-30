0bf767f33d6350e47d65cba912f577d98b888731
Rick Pfahl <pfahlr@gmail.com>
2019-07-30 13:44:20 -0400

    move ext4 into main script

--- scripts/cryptformat.sh
+++ scripts/cryptformat.sh
@@ -27,38 +27,55 @@ swapon -L swap
 echo "------------------------------------------------------"
 continueyn
 
-#make system partition 
+#make system partition
 echo "################ FORMATTING SYSTEM DISK #################"
 mkfs.btrfs --force --label system /dev/mapper/system
 echo "------------------------------------------------------"
 continueyn
 
+if [[ $system_part_type == "btrfs" ]]; then
+  ###BEGIN BTRFS SPECIFIC
+  echo "################ CREATING SUBVOLUMES ####################"
+  mount -t btrfs LABEL=system /mnt
+  btrfs subvolume create /mnt/root
+  btrfs subvolume create /mnt/home
+  btrfs subvolume create /mnt/snapshots
+  umount -R /mnt
+  echo "------------------------------------------------------"
+  continueyn
 
-###BEGIN BTRFS SPECIFIC
-echo "################ CREATING SUBVOLUMES ####################"
-mount -t btrfs LABEL=system /mnt
-btrfs subvolume create /mnt/root
-btrfs subvolume create /mnt/home
-btrfs subvolume create /mnt/snapshots
-umount -R /mnt
-echo "------------------------------------------------------"
-continueyn
+  echo "################# MOUNTING SUBVOLUMES ###################"
+  mount -t btrfs -o subvol=root,defaults,x-mount,compress=lzo,noatime LABEL=system /mnt
+  echo "root... \n"
+  mkdir /mnt/home
+  mount -t btrfs -o subvol=home,defaults,x-mount,mkdir,compress=lzo,noatime LABEL=system /mnt/home
 
-echo "################# MOUNTING SUBVOLUMES ###################"
-mount -t btrfs -o subvol=root,defaults,x-mount,compress=lzo,noatime LABEL=system /mnt
-echo "root... \n"
-mkdir /mnt/home
-mount -t btrfs -o subvol=home,defaults,x-mount,mkdir,compress=lzo,noatime LABEL=system /mnt/home
+  echo "home... \n"
+  mkdir /mnt/.snapshots
 
-echo "home... \n"
-mkdir /mnt/.snapshots
+  echo ".snapshots \n"
+  mount -t btrfs -o subvol=snapshots,defaults,x-mount,mkdir,compress=lzo,noatime LABEL=system /mnt/.snapshots
 
-echo ".snapshots \n"
-mount -t btrfs -o subvol=snapshots,defaults,x-mount,mkdir,compress=lzo,noatime LABEL=system /mnt/.snapshots
+  echo "------------------------------------------------------"
+  continueyn
+  ### END BTRFS SECTION
+elif [[ $system_part_type == "ext4" ]]; then
+  ###ext4  specific
+  #make system partition
+  echo "################ FORMATTING SYSTEM DISK #################"
+  mkfs.ext4 -F -L system /dev/mapper/system
 
-echo "------------------------------------------------------"
-continueyn
-### END BTRFS SECTION
+  echo "------------------------------------------------------"
+
+  echo "################ CREATING SUBVOLUMES ####################"
+  mount -t ext4 LABEL=system /mnt
+
+  echo "------------------------------------------------------"
+
+  mkdir /mnt/home /mnt/boot
+else
+  echo "filesystem $system_part_type not yet supported"
+fi
 
 echo "############### MOUNTING EFI PART #######################"
 mkdir /mnt/boot
