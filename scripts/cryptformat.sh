3210e2d174486aa6fa304d5a163ce130c4fc7f7b
Rick Pfahl <pfahlr@gmail.com>
2019-07-31 17:17:55 -0400

    cleanup

--- scripts/cryptformat.sh
+++ scripts/cryptformat.sh
@@ -2,52 +2,52 @@
 
 source _vars.inc.sh
 
-echo "################ FORMATTING EFI DISK #################"
+echo "|=============  FORMATTING EFI DISK  =========================================|"
 mkfs.fat -F32 -n EFI /dev/disk/by-partlabel/EFI
-echo "------------------------------------------------------"
+echo "-------------------------------------------------------------------------------"
 continueyn
 #emcrypt the system part
-echo "############ INITIALIZING SYSTEM ENCRYPTION ##########"
+echo "|=============  INITIALIZING SYSTEM ENCRYPTION  ==============================|"
 cryptsetup luksFormat --align-payload=8192 -s 256 -c aes-xts-plain64 /dev/disk/by-partlabel/ENCSYS
 mkfs.fat -F32 -n EFI /dev/disk/by-partlabel/EFI
-echo "------------------------------------------------------"
+echo "----------------------------------------==-------------------------------------"
 continueyn
 
 #open the encrypted disk
-echo "################ OPENING SYSTEM DISK  #################"
+echo "|=============  OPENING SYSTEM DISK  =========================================|"
 cryptsetup open /dev/disk/by-partlabel/ENCSYS system
-echo "------------------------------------------------------"
+echo "-------------------------------------------------------------------------------"
 continueyn
 
 #make encrypted swap
-echo "############# INITIALIZING SWAP ENCRYPTION ############"
+echo "|============= INITIALIZING SWAP ENCRYPTION ==================================|"
 cryptsetup open --type plain --key-file /dev/urandom  /dev/disk/by-partlabel/ENCSWAP swap
 mkswap -L swap /dev/mapper/swap
 swapon -L swap
-echo "------------------------------------------------------"
+echo "-------------------------------------------------------------------------------"
 echo "..."
 echo "... about to format system partition as $system_part_fstype"
 continueyn
 
 if [[ $system_part_fstype == "btrfs" ]]; then
 
+  #BEGIN BTRFS
   #make system partition
-  echo "################ FORMATTING SYSTEM DISK #################"
+  echo "|=============  FORMATTING SYSTEM DISK  ====================================|"
   mkfs.btrfs --force --label system /dev/mapper/system
-  echo "------------------------------------------------------"
+  echo "-----------------------------------------------------------------------------"
   continueyn
 
-  ###BEGIN BTRFS SPECIFIC
-  echo "################ CREATING SUBVOLUMES ####################"
+  echo "|=============  CREATING SUBVOLUMES  =======================================|"
   mount -t btrfs LABEL=system /mnt
   btrfs subvolume create /mnt/root
   btrfs subvolume create /mnt/home
   btrfs subvolume create /mnt/snapshots
   umount -R /mnt
-  echo "------------------------------------------------------"
+  echo "-----------------------------------------------------------------------------"
   continueyn
 
-  echo "################# MOUNTING SUBVOLUMES ###################"
+  echo "|=============  MOUNTING SUBVOLUMES  =======================================|"
   mount -t btrfs -o subvol=root,defaults,x-mount,compress=lzo,noatime LABEL=system /mnt
   echo "root... \n"
   mkdir /mnt/home
@@ -59,32 +59,36 @@ if [[ $system_part_fstype == "btrfs" ]]; then
   echo ".snapshots \n"
   mount -t btrfs -o subvol=snapshots,defaults,x-mount,mkdir,compress=lzo,noatime LABEL=system /mnt/.snapshots
 
-  echo "------------------------------------------------------"
+  echo "-----------------------------------------------------------------------------"
   continueyn
-  ### END BTRFS SECTION
+  ### END BTRFS ###
+
 elif [[ $system_part_fstype == "ext4" ]]; then
-  ###ext4  specific
+
+  #BEGIN EXT4
   #make system partition
-  echo "################ FORMATTING SYSTEM DISK #################"
+  echo "|=============  FORMATTING SYSTEM DISK  ====================================|"
   mkfs.ext4 -F -L system /dev/mapper/system
 
-  echo "------------------------------------------------------"
+  echo "-----------------------------------------------------------------------------"
 
-  echo "################ CREATING SUBVOLUMES ####################"
+  echo "|=============  CREATING SUBVOLUMES  =======================================|"
   mount -t ext4 LABEL=system /mnt
 
-  echo "------------------------------------------------------"
+  echo "-----------------------------------------------------------------------------"
 
   mkdir /mnt/home /mnt/boot
+  ### END EXT4 ###
+
 else
   echo "filesystem $system_part_fstype not yet supported"
 fi
 
-echo "############### MOUNTING EFI PART #######################"
+echo "|=============  MOUNTING EFI PART  ===========================================|"
 mkdir /mnt/boot
 mount LABEL=EFI /mnt/boot
-echo "------------------------------------------------------"
+echo "-------------------------------------------------------------------------------"
 continueyn
 
-echo "###########!!! FILESYSTEM BUILD COMPLETE !!!#############"
+echo "###########!!!  FILESYSTEM BUILD COMPLETE  !!!================================|"
 lsblk
